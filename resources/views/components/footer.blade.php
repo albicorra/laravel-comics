ee5990c8e8a022dcad0bf3b1a6f97b876b6ea40b
Alberto Corrado <alberto2001corrado@gmail.com>
2022-04-20 12:25:14 +0200

    file

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,65 @@
+<div class="footer-container">
+    <div class="container-info">
+        <div class="footer-info">
+
+            <div class="col-info">
+                <h3 class="title-footer">Dc Comics</h3>
+                <div class="item-footer">Characters</div>
+                <div class="item-footer">Comics</div>
+                <div class="item-footer">Movies</div>
+                <div class="item-footer">Tv</div>
+                <div class="item-footer">Games</div>
+                <div class="item-footer">Videos</div>
+                <div class="item-footer">News</div>
+
+                <div class="title-footer">Shop</div>
+                <div class="item-footer">Shop DC</div>
+                <div class="item-footer">Shop DC Collectibles</div>
+            </div>
+
+            <div class="col-info">
+                <h3 class="title-footer">Dc</h3>
+                <div class="item-footer">Terms Of Use</div>
+                <div class="item-footer">Privacy policy (new</div>
+                <div class="item-footer">ad Choices</div>
+                <div class="item-footer">Advertising</div>
+                <div class="item-footer">Jobs</div>
+                <div class="item-footer">Subscriptions</div>
+                <div class="item-footer">Talent Workshops</div>
+                <div class="item-footer">CPSC Certificates</div>
+                <div class="item-footer">Ratins</div>
+                <div class="item-footer">Shop Help</div>
+                <div class="item-footer">Contact Us</div>
+            </div>
+
+            <div class="col-info">
+                <h3 class="title-footer">Sites</h3>
+                <div class="item-footer">DC</div>
+                <div class="item-footer">DC UNIVERSE INFINITE</div>
+                <div class="item-footer">DC Kids</div>
+                <div class="item-footer">DC Shop</div>
+                <div class="item-footer">MAD Magazine</div>
+            </div>
+
+        </div>
+      
+    </div>
+</div>
+
+<div class="footer-social">
+    <div class="container">
+
+        <div class="sign-up">
+            Sign-Up Now
+        </div>
+
+        <div class="social">
+            <div>Follow Us</div>
+
+            <div class="icon" v-for="(el, i) in social" :key="i">
+                <img :src="el.icon" alt="">
+            </div>
+        </div>
+
+    </div>
+</div>
